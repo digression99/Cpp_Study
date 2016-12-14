fc7eb4da0270a31790ee15edc51a90c58870b613
digression99 <digression99@gmail.com>
2016-12-14 12:28:28 +0900

    Create rangebasedfor.cpp

---
+++ rangebasedfor.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+#include <vector>
+
+int main()
+{
+    vector<int> v;
+    
+    v.push_back(10);
+    v.push_back(20);
+    v.push_back(30);
+    v.push_back(40);
+    v.push_back(50);
+    
+    for (auto i : v)
+    {
+        cout << i << endl;
+    }
+    
+    return 0;
+}
